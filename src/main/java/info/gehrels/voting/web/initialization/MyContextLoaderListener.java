ec4a7e3f14b7ce9928873505b23bb61263fa67c3
Benjamin Gehrels <github@gehrels>
2013-08-24 16:30:37 +0200

    Added a CharacterEncoding Filter
    Browsers do not send the content encoding of posted forms to the server. They silently encode it using the encoding of the source page. Since we deliver all Pages as UTF-8, the most probable request encoding of posted forms will also be UTF-8.

--- src/main/java/info/gehrels/voting/web/initialization/MyContextLoaderListener.java
+++ src/main/java/info/gehrels/voting/web/initialization/MyContextLoaderListener.java
@@ -1,10 +1,13 @@
 package info.gehrels.voting.web.initialization;
 
+import info.gehrels.voting.web.CharacterEncodingFilter;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContextEvent;
+import java.util.EnumSet;
 
 public class MyContextLoaderListener extends ContextLoaderListener {
 	public MyContextLoaderListener() {
@@ -22,5 +25,7 @@ public class MyContextLoaderListener extends ContextLoaderListener {
 		super.contextInitialized(event);
 
 		event.getServletContext().getServletRegistrations().get("jsp").addMapping("/WEB-INF/jsp/*");
+		event.getServletContext().addFilter("characterEncodingFilter", new CharacterEncodingFilter())
+			.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), false, "/*");
 	}
 }

290892316593d7ae86a1d35f6a2b190a60b703ee
Benjamin Gehrels <github@gehrels>
2013-08-24 16:44:58 +0200

    Generating and storing complete Ballots on submission of the castVote form

--- src/main/java/info/gehrels/voting/web/CastVoteController.java
+++ src/main/java/info/gehrels/voting/web/CastVoteController.java
@@ -1,14 +1,19 @@
 package info.gehrels.voting.web;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSet.Builder;
+import com.google.common.collect.Iterables;
 import info.gehrels.voting.Ballot;
 import info.gehrels.voting.Ballot.ElectionCandidatePreference;
 import info.gehrels.voting.genderedElections.GenderedCandidate;
+import info.gehrels.voting.genderedElections.GenderedElection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
@@ -23,23 +28,43 @@ public class CastVoteController {
 		this.castBallotsState = castBallotsState;
 	}
 
-	@RequestMapping(value = {"/castVote"}, method = {POST, PUT})
+	@RequestMapping(value = "/castVote", method = {POST, PUT})
 	public String doCastVote(@ModelAttribute CastVoteForm castVoteForm) {
-		castBallotsState.castBallotsById.put(castVoteForm.getBallotId(), new Ballot<>(
-			ImmutableSet.of(
-				new ElectionCandidatePreference<>(ballotLayoutState.ballotLayout.getElections().get(0),
-				                                  ImmutableSet.<GenderedCandidate>of()))));
+		castBallotsState.castBallotsById.put(castVoteForm.getBallotId(), createBallotFromForm(castVoteForm));
 
 		return "redirect:/castVote";
 	}
 
-	@RequestMapping("/castVote")
+	@RequestMapping(value = "/castVote", method = {HEAD, GET})
 	public ModelAndView doGet() {
 		if (ballotLayoutState.ballotLayout == null) {
 			return new ModelAndView("redirect:/");
 		}
 
 		return new ModelAndView("castVote", "ballotLayout", ballotLayoutState.ballotLayout);
+	}
+
+	private Ballot<GenderedCandidate> createBallotFromForm(CastVoteForm castVoteForm) {
+		int i = 0;
+		Builder<ElectionCandidatePreference<GenderedCandidate>> preferenceSetBuilder = ImmutableSet.builder();
+		for (GenderedElection genderedElection : ballotLayoutState.ballotLayout.getElections()) {
+			ElectionCandidatePreference<GenderedCandidate> preference = createPreference(genderedElection, castVoteForm
+				.getVotesByElectionId().get(i));
+			preferenceSetBuilder.add(preference);
+			i++;
+		}
+
+		return new Ballot<>(preferenceSetBuilder.build());
+	}
+
+	private ElectionCandidatePreference<GenderedCandidate> createPreference(GenderedElection genderedElection,
+	                                                                        String preferenceString) {
+		Builder<GenderedCandidate> preferenceBuilder = ImmutableSet.builder();
+		for (char c : preferenceString.toUpperCase().toCharArray()) {
+			int candidateIndex = c-'A';
+			preferenceBuilder.add(Iterables.get(genderedElection.getCandidates(), candidateIndex));
+		}
+		return new ElectionCandidatePreference<>(genderedElection, preferenceBuilder.build());
 	}
 
 }

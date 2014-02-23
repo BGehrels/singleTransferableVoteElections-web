b4c2e062267794890800a64e0d9452916c561eb1
Benjamin Gehrels <github@gehrels>
2014-02-23 16:29:54 +0100

    Better Wording

--- src/main/java/info/gehrels/voting/web/ElectionCalculationState.java
+++ src/main/java/info/gehrels/voting/web/ElectionCalculationState.java
@@ -1,10 +1,10 @@
 package info.gehrels.voting.web;
 
 public enum ElectionCalculationState {
-	NOT_YET_STARTED("Die Ergebnisberechnung wird in Kürze starten"),
+	NOT_YET_STARTED("Die Ergebnisberechnung wurde noch nicht gestartet."),
 	RUNNING("Die Wahlergebnisse werden momentan berechnet. Bitte laden Sie diese Seite in ein paar Sekunden neu."),
 	MANUAL_AMBIGUITY_RESOLUTION_NECCESSARY(
-		"Mehrere Kandidierende haben die Selbe Stimmzahl. Eine Manuelle Auswahl ist notwendig."),
+		"Mehrere Kandidierende haben die selbe Stimmzahl. Eine Manuelle Auswahl ist notwendig."),
 	FINISHED("Die Berechnung der Wahlergebnisse wurde erfolgreich abgeschlossen.");
 
 	private final String description;

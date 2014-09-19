79735256b3cff503702f83c46326a5a63910dd05
Benjamin Gehrels <github@gehrels>
2014-09-19 21:46:56 +0200

    Printing the URL under which this server may be reached on startup

--- src/main/java/info/gehrels/voting/web/ServerStartedMessagePrintingListener.java
+++ src/main/java/info/gehrels/voting/web/ServerStartedMessagePrintingListener.java
@@ -5,11 +5,28 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 class ServerStartedMessagePrintingListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
 	private static final Logger LOG = LoggerFactory.getLogger("ServerStarted");
 
 	@Override
 	public void onApplicationEvent(EmbeddedServletContainerInitializedEvent o) {
-		LOG.info("Die Wahlauszählung kann beginnen. Die Eingabemaske ist nun unter http://localhost:{}/ erreichbar.", o.getSource().getPort());
+		LOG.info("Die Wahlauszählung kann beginnen. Die Eingabemaske ist nun unter den folgenden URLs erreichbar:",
+		         o.getSource().getPort());
+		try {
+			InetAddress localHost = InetAddress.getLocalHost();
+			logUrl(o, localHost.getCanonicalHostName());
+			logUrl(o, localHost.getHostAddress());
+			logUrl(o, localHost.getHostName());
+		} catch (UnknownHostException e) {
+			logUrl(o, "localhost");
+			logUrl(o, "127.0.0.1");
+		}
+	}
+
+	private void logUrl(EmbeddedServletContainerInitializedEvent o, String host) {
+		LOG.info("http://{}:{}/", host, o.getEmbeddedServletContainer().getPort());
 	}
 }
